3fb20bb99173be8276e660aca0d18c332a6a7c5a
Gabe <24978329+EarthenSky@users.noreply.github.com>
2025-03-06 16:25:16 -0800

    Update fresh_setup.sh

--- fresh_setup.sh
+++ fresh_setup.sh
@@ -46,6 +46,8 @@ python3 -m venv /opt/certbot
 /opt/certbot/bin/pip install --upgrade pip
 /opt/certbot/bin/pip install certbot certbot-nginx
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+# NEW: add certbot (pip) as a cronjob
+echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && sudo certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
 
 echo "----"
 echo "add user csss_site..."
