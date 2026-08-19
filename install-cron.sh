e58448744550ea15db10d28b401133c34bda6b46
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-19 03:19:54 -0700

    feat(cron): add user_session cleaner

--- install-cron.sh
+++ install-cron.sh
@@ -16,6 +16,9 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # Clean expired CAS authentication redirects every 15 minutes
 */15 * * * * csss-site /usr/bin/psql main -c "DELETE FROM auth_redirect WHERE expires_at < NOW();" >/dev/null 2>&1
 
+# Clean expired user sessions at 3AM everyday
+0 3 * * * csss-site /usr/bin/psql main -c "DELETE FROM user_session WHERE expires_at < NOW();" >/dev/null 2>&1
+
 # Example future jobs:
 # 0 3 * * * csss-site /path/to/some-maintenance-script.sh
 # 0 4 * * 0 csss-site /path/to/weekly-cleanup.sh
